416feef47a9d3e36a0570100c788a895d364f57a
Leon <lw75251@gmail.com>
2019-03-07 12:13:27 -0800

    Added keys as ENV Variables on Heroku

--- configs/firebaseconfig.ts
+++ configs/firebaseconfig.ts
@@ -2,7 +2,8 @@ import { Firestore } from "@google-cloud/firestore";
 import * as admin from "firebase-admin";
 
 /** Firebase Documentation - Config FIREBASE */
-const serviceAccount = require("../../divvy-d1a54-firebase-adminsdk-6zgy9-660c566562.json");
+// const serviceAccount = require("../../divvy-d1a54-firebase-adminsdk-6zgy9-660c566562.json");
+const serviceAccount = process.env.serviceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
