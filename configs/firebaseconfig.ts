44f13977af27e2b5789ba6b9ef99d2ee1ec7e8be
Leon <lw75251@gmail.com>
2019-03-07 12:52:43 -0800

    Adds more ENV Variable to Heroku

--- configs/firebaseconfig.ts
+++ configs/firebaseconfig.ts
@@ -3,11 +3,24 @@ import * as admin from "firebase-admin";
 
 /** Firebase Documentation - Config FIREBASE */
 // const serviceAccount = require("../../divvy-d1a54-firebase-adminsdk-6zgy9-660c566562.json");
-const serviceAccount = process.env.serviceAccount;
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://divvy-d1a54.firebaseio.com"
+// });
+
+const PROJECT_ID = process.env.PROJECT_ID;
+const CLIENT_EMAIL = process.env.CLIENT_EMAIL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://divvy-d1a54.firebaseio.com"
+  credential: admin.credential.cert({
+    projectId: PROJECT_ID,
+    clientEmail: CLIENT_EMAIL,
+    privateKey: PRIVATE_KEY
+  }),
+  databaseURL: DATABASE_URL
 });
 
 export const db: Firestore = admin.firestore();
